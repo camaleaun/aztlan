d5b8ae5a0a5ffb4556fad31b3c6a14399a198a46
Eduardo Pittol <edpittol@gmail.com>
2019-08-29 11:26:17 -0300

    WIP: Criando comando de build de imagens Docker

---
+++ environment/bin/shared/docker_compose.sh
@@ -0,0 +1,35 @@
+#######################################
+# Run docker-compose following Aztlan
+# structure
+# Arguments:
+#   The docker-compose command
+# Returns:
+#   None
+#######################################
+docker_compose() {
+	# Load project variables
+	. $(dirname $BASH_SOURCE)/aztlan_variables.sh
+
+  # Check if the docker-compose command exists
+  if [ ! $( command -v docker-compose ) ]; then
+    >&2 echo "Command docker-compose doesn't exist"
+    return
+  fi
+
+  COMPOSE="docker-compose -p ${PROJECT} -f ${ENV_ROOT_PATH}/environment/docker-compose.yml"
+  [ 'Darwin' = ${OS} ] && COMPOSE+=" -f ${ENV_ROOT_PATH}/environment/docker-compose.mac.yml"
+
+  VOLUME_PREFIX=${VOLUME_PREFIX} ${COMPOSE} $@
+}
+
+#######################################
+# Run docker-compose dist configuration
+# Arguments:
+#   The docker-compose command
+# Returns:
+#   None
+#######################################
+docker_compose_dist() {
+  . ./environment/env/build.env
+  docker-compose -f environment/docker-compose.dist.yml $@
+}
