a5dd736f55ccf3f42e713d3ba75b3f6461fa9be9
Eduardo Pittol <edpittol@gmail.com>
2019-09-11 10:27:08 -0300

    WIP: Refatoração do carregamento dos assets e integração com internacionalização dos blocos

--- inc/src/setup/class-assets.php
+++ inc/src/setup/class-assets.php
@@ -8,7 +8,6 @@
 namespace Aztec\Setup;
 
 use Aztec\Base;
-use DI\Container;
 
 /**
  * Add Scripts and Styles
@@ -18,10 +17,11 @@ class Assets extends Base {
 	 * Add hooks
 	 */
 	public function init() {
-		add_action( 'wp_enqueue_scripts', $this->callback( 'enqueue_styles' ), 1 );
-		add_action( 'wp_enqueue_scripts', $this->callback( 'enqueue_script' ) );
-
 		add_action( 'enqueue_block_editor_assets', $this->callback( 'enqueue_block_editor_assets' ) );
+		add_action( 'init', $this->callback( 'register_scripts' ) );
+		add_action( 'init', $this->callback( 'register_styles' ) );
+		add_action( 'wp_enqueue_scripts', $this->callback( 'enqueue_styles' ), 1 );
+		add_action( 'wp_enqueue_scripts', $this->callback( 'enqueue_scripts' ) );
 	}
 
 	/**
@@ -35,36 +35,51 @@ class Assets extends Base {
 	}
 
 	/**
-	 * Load application style
+	 * Register all application scripts
+	 */
+	public function register_scripts() {
+		wp_register_script( 'aztlan-editor', $this->assets_uri( 'editor.js' ), [], self::VERSION, true );
+		wp_register_script( 'aztlan-vendor', $this->assets_uri( 'vendor.js' ), array( 'jquery' ), self::VERSION, true );
+		wp_register_script( 'aztlan', $this->assets_uri( 'app.js' ), array(), self::VERSION, true );
+	}
+
+	/**
+	 * Register all application styles
+	 */
+	public function register_styles() {
+		wp_register_style( 'aztlan-editor', $this->assets_uri( 'editor.css' ), [], self::VERSION );
+		wp_register_style( 'aztlan', $this->assets_uri( 'app.css' ), array(), self::VERSION );
+	}
+
+	/**
+	 * Set the translation for scripts that use wp.i18n
+	 */
+	public function set_script_translations() {
+		wp_set_script_translations( 'aztlan-editor', 'aztlan', ABSPATH . '../../assets/languages' );
+	}
+
+	/**
+	 * Load website styles
 	 */
 	public function enqueue_styles() {
-		wp_enqueue_style( 'aztec-env', $this->assets_uri( 'app.css' ), array(), self::VERSION );
+		wp_enqueue_style( 'aztlan' );
 	}
 
 	/**
-	 * Load application script
+	 * Load website scripts
 	 */
-	public function enqueue_script() {
-		wp_enqueue_script( 'aztec-env-vendor', $this->assets_uri( 'vendor.js' ), array( 'jquery' ), self::VERSION, true );
-		wp_enqueue_script( 'aztec-env-app', $this->assets_uri( 'app.js' ), array(), self::VERSION, true );
-
-		// Append PHP data to JS.
-		wp_localize_script(
-			'aztec-env-app',
-			'aztec_env',
-			array(
-				'ajax_url' => admin_url( 'admin-ajax.php' ),
-			)
-		);
+	public function enqueue_scripts() {
+		wp_enqueue_script( 'aztlan-vendor' );
+		wp_enqueue_script( 'aztlan' );
 	}
 
 	/**
-	 * Enqueue block editor assets.
+	 * Enqueue block editor assets
 	 */
 	public function enqueue_block_editor_assets() {
-		wp_enqueue_script( 'aztec-env-vendor', $this->assets_uri( 'vendor.js' ), [], self::VERSION, true );
-		wp_enqueue_script( 'aztec-env-editor', $this->assets_uri( 'editor.js' ), [], self::VERSION, true );
+		wp_enqueue_script( 'aztlan-vendor' );
+		wp_enqueue_script( 'aztlan-editor' );
 
-		wp_enqueue_style( 'aztec-env-editor', $this->assets_uri( 'editor.css' ), [], self::VERSION );
+		wp_enqueue_style( 'aztlan-editor' );
 	}
 }
